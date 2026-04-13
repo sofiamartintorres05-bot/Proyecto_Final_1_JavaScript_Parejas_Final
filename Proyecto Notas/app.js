49df1141be3f7ba43331688c7e4e3eab9ddb2ad7
marianacastellanosparra <marianacastellanosparra@gmail.com>
2026-04-13 18:53:57 -0500

    feat: mostrar aprendices con forEach

--- Proyecto Notas/app.js
+++ Proyecto Notas/app.js
@@ -4,3 +4,7 @@ const aprendices = [
   { nombre: "Carlos", nota: 2.8, programa: "ADSO" },
   { nombre: "Maria", nota: 4.8, programa: "ADSO" }
 ];
+
+function mostrar() {
+  aprendices.forEach(a => console.log(a));
+}
